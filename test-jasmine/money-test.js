971308e2750ee8cb9f3360429e8704255e0931ef
Jl Sut Pi <sutpi070@gmail.com>
2025-10-26 20:25:01 +0700

    remove default test and add moeny test

---
+++ test-jasmine/money-test.js
@@ -0,0 +1,16 @@
+import { showMoney } from "../scripts/utils/money.js";
+
+describe("test suite : Format currency", () => {
+  it("convert cents into dollars", () => {
+    expect(showMoney(2095)).toEqual("20.95");
+  });
+  it("work with 0", () => {
+    expect(showMoney(0)).toEqual("0.00");
+  });
+  it("rounds up to the nearest cent", () => {
+    expect(showMoney(2000.5)).toEqual("20.01");
+  });
+  it("rounds down to the nearest cent", () => {
+    expect(showMoney(2000.4)).toEqual("20.00");
+  });
+});
