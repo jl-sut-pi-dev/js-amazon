baa6308a2c2a7c064bc7b7120bc9e4cf92672d18
Jl Sut Pi <sutpi070@gmail.com>
2025-10-29 12:56:37 +0700

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCheckoutHeader } from "./checkout/checkout-header.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPayMentSummary } from "./checkout/paymentSummary.js";
-import "../data/cart-oop.js";
+import "../data/cart-calss.js";
 renderCheckoutHeader();
 renderOrderSummary();
 renderPayMentSummary();
